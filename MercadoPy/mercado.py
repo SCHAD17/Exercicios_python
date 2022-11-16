d72e832efb8b9a5cdd473890383057c273823f40
SCHADRAC WANZA ISULA <49597505+SCHAD17@users.noreply.github.com>
2022-11-16 16:00:33 -0300

    Criando as primeiras conteudos do mercados

--- MercadoPy/mercado.py
+++ MercadoPy/mercado.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+from typing import List, Dict
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from models.produto import Produto
+from utils.helper import formata_float_str_moeda
+
+produtos: List[Produto] = []
+carrinho: List[Dict[produtos, int]] = []
+
+def main() -> None:
+    menu()
+
+def menu() -> None:
+    pass
+
+def cadastrar_produto() -> None:
+    pass
+
+def listar_produto() -> None:
+    pass
+
+def comprar_produto() -> None:
+    pass
+
+def visualizae_carrinho() -> None:
+    pass
+
+def fechar_pedido() -> None:
+    pass
+
+def pega_produto_por_codigo(codigo: int) -> Produto:
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ =='__main__':
+    main()
