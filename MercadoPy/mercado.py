e179c5c22d044bb3444390015a895709b5c91a2b
SCHADRAC WANZA ISULA <49597505+SCHAD17@users.noreply.github.com>
2022-11-16 16:11:25 -0300

    Criando conteudo da função main()

--- MercadoPy/mercado.py
+++ MercadoPy/mercado.py
@@ -11,7 +11,39 @@ def main() -> None:
     menu()
 
 def menu() -> None:
-    pass
+    print('===============================================')
+    print('================Bem-vindo(a)===================')
+    print('================Goshen Shop====================')
+    print('===============================================')
+
+    print('Selecione uma opção abaixo: ')
+    print('1 - Cadastrar produto')
+    print('2 - Listar produto')
+    print('3 - Comprar produto')
+    print('4 - Visualizar carrinho')
+    print('5 - Fechar pedido')
+    print('6 - Sair do sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        cadastrar_produto()
+    elif opcao == 2:
+        listar_produto()
+    elif opcao == 3:
+        comprar_produto()
+    elif opcao == 4:
+        visualizae_carrinho()
+    elif opcao == 5:
+        fechar_pedido()
+    elif opcao == 6:
+        print('Volte sempre')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida!')
+        sleep(1)
+        menu()
 
 def cadastrar_produto() -> None:
     pass
